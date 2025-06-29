842ee37d1581c0830be2e0a01c66d95d0c42643c
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-06-29 22:45:08 +0530

    Update contains-duplicate.c

--- arrays_hashing/contains-duplicate.c
+++ arrays_hashing/contains-duplicate.c
@@ -1,11 +1,16 @@
 //217 - contains duplicate
+//Runtime = 41ms, Beats = 75.95%
+
+//this function returns the relation between numbers a and b
 int compare(const void *a, const void *b) {
     return (*(int*)a - *(int*)b);
 }
 
 bool containsDuplicate(int* nums, int numsSize) {
+    //we sort the array using quick sort
     qsort(nums, numsSize, sizeof(nums[0]), compare);
     for(int i=0; i<numsSize-1; i++) {
+        //check the adjacent array elements to find any duplicates
         if(nums[i] == nums[i+1])
             return true;
     }
