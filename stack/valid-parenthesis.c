385775503780f5cd2e4b2c6489b8b4649684a910
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-07-01 23:54:29 +0530

    Add C solution to Valid Parenthesis

---
+++ stack/valid-parenthesis.c
@@ -0,0 +1,92 @@
+//20 - Valid Parenthesis
+//Runtime = 0ms, Beats = 100%
+
+typedef struct {
+    char *arr;
+    int sp;
+    int capacity;
+}Stack;
+void init(Stack *st, int size) {
+    st->arr = (char*)malloc(sizeof(char) * size);
+    st->sp = -1;
+    st->capacity = size;
+}
+bool isEmpty(Stack *st) {
+    if (st->sp == -1)
+        return true;
+    else
+        return false;
+}
+void push(Stack *st, char ele) {
+    if(st->sp < st->capacity-1) {
+        st->sp++;
+        st->arr[st->sp] = ele;
+    }
+    else {
+        printf("overflow!");
+    }
+}
+char pop(Stack *st) {
+    char ele = '\0';
+    if(!isEmpty(st)) {
+        ele = st->arr[st->sp];
+        st->sp--;
+    }
+    return ele;
+}
+bool process(char* s) {
+    Stack st;
+    int len = strlen(s);
+    init(&st, len);
+    int i=0;
+    while(s[i] != '\0') {
+	//whenever there's an opening parenthesis, we push into stack
+        if((s[i]=='(') || (s[i]=='[') || (s[i]=='{')) {
+            push(&st, s[i]);
+        }
+	//we pop the top stack element if there's a closing parenthesis
+	//but this is only done when the closing and stack top character are of the same kind!
+        else if(s[i]==')') {
+            if(!isEmpty(&st) && st.arr[st.sp] == '(')
+                pop(&st);
+            else {
+                free(st.arr);
+                return false;
+            }
+        }
+        else if(s[i]=='}') {
+            if(!isEmpty(&st) && st.arr[st.sp] == '{')
+                pop(&st);
+            else {
+                free(st.arr);
+                return false;
+            }
+        }
+        else if(s[i]==']') {
+            if(!isEmpty(&st) && st.arr[st.sp] == '[')
+                pop(&st);
+            else {
+                free(st.arr);
+                return false;
+            }
+        }
+        else {
+            printf("Invalid character!");
+            free(st.arr);
+            return false;
+        }
+        i++;
+    }
+    if(isEmpty(&st)) {
+        free(st.arr);
+        return true;
+    }
+    else {
+        free(st.arr);
+        return false;
+    }
+}
+
+bool isValid(char* s) {
+    return process(s);
+}
