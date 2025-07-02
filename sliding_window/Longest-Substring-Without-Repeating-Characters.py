d9c7c3ba6ce9c5bcc594a8d0c066988af0f07d4c
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-07-02 23:48:22 +0530

    Update Longest-Substring-Without-Repeating-Characters.py

--- sliding_window/Longest-Substring-Without-Repeating-Characters.py
+++ sliding_window/Longest-Substring-Without-Repeating-Characters.py
@@ -1,18 +1,18 @@
-//3 - Longest Substring Without Repeating Characters
-//Runtime = 23ms, Beats = 61.82%
+# 3 - Longest Substring Without Repeating Characters
+# Runtime = 23ms, Beats = 61.82%
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        start = 0
-        seen = {}
-        max_length=0
+        start = 0    # tracks beginning to current substring
+        seen = {}    # dictionary to track last "seen" positions
+        max_length=0    # updates when new valid window is found
         for i in range(len(s)):
-            if(s[i] in seen and seen[s[i]] >= start):
-                start = seen[s[i]] + 1
-            seen[s[i]] = i
+            if(s[i] in seen and seen[s[i]] >= start):    # duplicate found
+                start = seen[s[i]] + 1    # move start after the duplicate
+            seen[s[i]] = i    # update last seen index
             max_length = max(max_length, i-start+1)
         return max_length
         
