88dc76dd3c32cccdea82a05d4c2339d2e05700b6
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-07-05 22:18:59 +0530

    Update generate-parenthesis.py

--- stack/generate-parenthesis.py
+++ stack/generate-parenthesis.py
@@ -1,17 +1,17 @@
-// 22 - Generate Parenthesis
-// Runtime = 0ms, Beats = 100%
+# 22 - Generate Parenthesis
+# Runtime = 0ms, Beats = 100%
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
 
         def backtrack(current, open_counter, close_counter):
-            if(len(current) == 2*n):
+            if(len(current) == 2*n):    # the combination has been made, so we stop adding elements to it and return
                 result.append(current)
                 return
-            if(open_counter < n):
+            if(open_counter < n):    # number of opening parenthesis should always be less than or equal to given number
                 backtrack(current + '(', open_counter+1, close_counter)
-            if(close_counter < open_counter):
+            if(close_counter < open_counter):    # at any point, there shouldn't be more closing parenthesis than opening
                 backtrack(current + ')', open_counter, close_counter+1)
         
         backtrack('', 0, 0)
