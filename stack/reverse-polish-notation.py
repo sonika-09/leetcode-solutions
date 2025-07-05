c522d4d30f7e8c8eef62f65d584372221d55c53a
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-07-05 22:21:08 +0530

    Update reverse-polish-notation.py

--- stack/reverse-polish-notation.py
+++ stack/reverse-polish-notation.py
@@ -1,5 +1,5 @@
-// 150 - Evaluate Reverse Polish Notation
-// Runtime = 0ms, Beats = 100%
+# 150 - Evaluate Reverse Polish Notation
+# Runtime = 0ms, Beats = 100%
 
 class Solution(object):
     def evalRPN(self, tokens):
@@ -10,7 +10,7 @@ class Solution(object):
         stack = []
         for c in tokens:
             if (c=='+' or c=='-' or c=='*' or c=='/'):
-                ele1 = stack.pop()
+                ele1 = stack.pop()    # we pop 2 elements to perform the operation
                 ele2 = stack.pop()
                 if(c=='+'):
                     stack.append(ele2 + ele1)
@@ -19,8 +19,8 @@ class Solution(object):
                 elif(c=='*'):
                     stack.append(ele2 * ele1)
                 else:
-                    stack.append(int(ele2/ele1))
+                    stack.append(int(ele2/ele1))    # the result of division should not be a float
             else:
-                stack.append(int(c))    #to be able to push operations, we need to convert these strings into integers
+                stack.append(int(c))    # to be able to push operations, we need to convert these strings into integers
         return stack[-1]
         
