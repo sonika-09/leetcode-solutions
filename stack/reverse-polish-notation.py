5d30caff9b55cfd2042353285ba19e102877b2f9
Sonika Shetty <sonika.shetty2005@gmail.com>
2025-07-05 22:11:30 +0530

    Add Python Solution to Evaluate Reverse Polish Notation

---
+++ stack/reverse-polish-notation.py
@@ -0,0 +1,26 @@
+// 150 - Evaluate Reverse Polish Notation
+// Runtime = 0ms, Beats = 100%
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for c in tokens:
+            if (c=='+' or c=='-' or c=='*' or c=='/'):
+                ele1 = stack.pop()
+                ele2 = stack.pop()
+                if(c=='+'):
+                    stack.append(ele2 + ele1)
+                elif(c=='-'):
+                    stack.append(ele2 - ele1)
+                elif(c=='*'):
+                    stack.append(ele2 * ele1)
+                else:
+                    stack.append(int(ele2/ele1))
+            else:
+                stack.append(int(c))    #to be able to push operations, we need to convert these strings into integers
+        return stack[-1]
+        
